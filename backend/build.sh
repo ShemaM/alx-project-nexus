766602c0a5336e4cbb71ea9916c87cb7e860564d
Shema Manasseh <shemamanase992@gmail.com>
2026-02-06 20:16:23 +0300

    Update default superuser creation in build script

--- backend/build.sh
+++ backend/build.sh
@@ -12,4 +12,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # ⬇ Create a default admin user if it doesn't exist
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='superadmin').exists() or User.objects.create_superuser('superadmin', 'shemamanase992@gmail.com', 'Nm&&668852')"
