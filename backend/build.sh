54efd2781562bc01860371fd75d4c79c539b7e75
Shema Manasseh <shemamanase992@gmail.com>
2026-02-06 19:06:17 +0300

    Add auto-create superuser script

--- backend/build.sh
+++ backend/build.sh
@@ -10,3 +10,6 @@ python manage.py collectstatic --no-input
 
 # Run database migrations
 python manage.py migrate
+
+# ⬇ Create a default admin user if it doesn't exist
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
